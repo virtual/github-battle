b08e3355587403180f0bf5db1a6934a31c3edbcb
Jeanine Schoessler <jeanine.mt@gmail.com>
2016-10-29 09:05:03 -0600

    change to app using pure functions to display github data

--- app/index.js
+++ app/index.js
@@ -1,12 +1,51 @@
+var USER_DATA = {
+name: 'Jeanine Schoessler',
+username: 'virtual',
+image: 'https://avatars1.githubusercontent.com/u/93795?v=3&s=466'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
-    render: function () {
+var ProfilePic = React.createClass({
+    render: function() {
         return (
-            <div>Hello {this.props.name}!</div>
+            <img src={this.props.imageUrl} alt={this.props.username} style={{height:100, width:100}} />
         )
     }
 });
 
-ReactDOM.render(<Hello name="Jeanine" />, document.getElementById('app'));
+var ProfileLink = React.createClass({
+    render: function() {
+        return (
+        <div>
+         <a href={'http://github.com/' + this.props.username}>
+         {this.props.username}
+         </a>
+        </div>
+        )
+    }
+});
+
+var ProfileName = React.createClass({
+    render: function() {
+        return (
+            <div>{this.props.name}</div>
+        )
+    }
+});
+
+var Avatar = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
+            </div>
+        )
+    } 
+
+});
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
