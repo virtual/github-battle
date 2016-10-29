dfdd31de0838679d22b85f925d3056e50cba9d9a
Jeanine Schoessler <jeanine.mt@gmail.com>
2016-10-29 10:08:49 -0600

    add in react routers

--- app/index.js
+++ app/index.js
@@ -1,51 +1,8 @@
-var USER_DATA = {
-name: 'Jeanine Schoessler',
-username: 'virtual',
-image: 'https://avatars1.githubusercontent.com/u/93795?v=3&s=466'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var ProfilePic = React.createClass({
-    render: function() {
-        return (
-            <img src={this.props.imageUrl} alt={this.props.username} style={{height:100, width:100}} />
-        )
-    }
-});
-
-var ProfileLink = React.createClass({
-    render: function() {
-        return (
-        <div>
-         <a href={'http://github.com/' + this.props.username}>
-         {this.props.username}
-         </a>
-        </div>
-        )
-    }
-});
-
-var ProfileName = React.createClass({
-    render: function() {
-        return (
-            <div>{this.props.name}</div>
-        )
-    }
-});
-
-var Avatar = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <ProfilePic imageUrl={this.props.user.image} />
-                <ProfileName name={this.props.user.name} />
-                <ProfileLink username={this.props.user.username} />
-            </div>
-        )
-    } 
-
-});
-
-ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
+ReactDOM.render(
+    routes, 
+    document.getElementById('app')
+    );
