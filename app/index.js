36bae5488a86842fadc28ec9ed2351c0ad0cbc43
Jeanine Schoessler <jeanine.mt@gmail.com>
2016-10-29 15:03:45 -0600

    add in sentry

--- app/index.js
+++ app/index.js
@@ -1,6 +1,25 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes');
+var Raven = require('raven-js');
+
+
+var sentryKey = '78ea4f3797c645e3b0aef467096ac3bd';
+var sentryApp = '110458';
+var sentryURL = 'https://' + sentryKey + '@sentry.io/' + sentryApp;
+
+var _APP_INFO = {
+    name: 'Github Battle',
+    branch: 'video4',
+    version: '1.0'
+}
+
+Raven.config(sentryURL, {
+    release: _APP_INFO.version,
+    tags: {
+        branch: _APP_INFO.branch
+    }
+}).install();
 
 ReactDOM.render(
     routes, 
