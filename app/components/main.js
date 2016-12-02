71e925fced188cef3beb9f8ee3cc8cbfba08a31b
Jeanine Schoessler <jeanine.mt@gmail.com>
2016-12-02 16:12:49 -0700

    add css transitions between routes

--- app/components/main.js
+++ app/components/main.js
@@ -1,11 +1,20 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+require('../main.css'); // power of webpack
 
 var Main = React.createClass({
-    render: function() {
+    render: function () {
         return (
-            <div className='main-container'> 
-            {this.props.children}
-             </div>
+            <div className='main-container'>
+                <ReactCSSTransitionGroup
+                    transitionName="appear"
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={500} >
+                         
+                        {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+                        
+                </ReactCSSTransitionGroup>
+            </div>
         )
     }
 });
