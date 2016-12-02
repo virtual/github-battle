bb4c2bd4ab09f4363c3d0bacae0050ed50335a91
Jeanine Schoessler <jeanine.mt@gmail.com>
2016-12-02 15:55:47 -0700

    Add loading component

--- app/components/Results.js
+++ app/components/Results.js
@@ -5,6 +5,7 @@ var UserDetailsWrapper = require('./UserDetailsWrapper');
 var UserDetails = require('./UserDetails');
 var Link = require('react-router').Link;
 var MainContainer = require('./MainContainer');
+var Loading = require('./Loading')
 
 function puke(obj) {
     return <pre>{JSON.stringify(obj,2, ' ')}</pre>
@@ -24,7 +25,7 @@ function Results(props) {
 
     if (props.isLoading === true) {
         return (
-            <p>Loading...</p>
+            <Loading />
         )
     }
 
